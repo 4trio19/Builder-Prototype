c1a98d728174f49d4d210c393dc829e8553d79d9
4rio19 <nburressivc@gmail.com>
2019-08-08 22:18:14 -0500

    start work on tabs

--- src/components/CodeBox.js
+++ src/components/CodeBox.js
@@ -1,17 +1,10 @@
 import React from 'react'
-
+import CodeTabs from './CodeTabs';
 export default function CodeBox(props) {
-  console.log(props);
-  let toPrint = JSON.stringify(props.details, null, 2);
   return (
     // This needs work to make the stripe object properly
     <div className="code-box container">
-      <h4>Stripe JSON</h4>
-      <pre>
-        <code>
-          {toPrint}
-        </code>
-      </pre>
+      <CodeTabs details={props.details} />
     </div>
   )
 }
---
+++ src/components/CodeTabs.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+
+export default class CodeTabs extends Component {
+  state = {
+    tabIndex: 0
+  }
+  
+  changeTab = (e) => {
+    console.log(e.target.dataset.index);
+    e.preventDefault();
+    let currentIndex = parseInt(e.target.dataset.index);
+    if (currentIndex !== this.state.tabIndex) {
+      this.setState({
+        tabIndex:currentIndex
+      });
+    }
+    console.log(this.state.tabIndex);
+  }
+  render(props) {
+    console.log(props);
+    let toPrint = JSON.stringify(this.props.details, null, 2);
+    return (
+      <div>
+        <div className="container">
+          <div className="card">
+            <div className="card-header">
+              <ul className="nav nav-pills card-header-pills">
+                <li className="nav-item">
+                  <button className={"nav-link code-tab btn " + (this.state.tabIndex === 0 ? 'active': null)} data-index="0" onClick={this.changeTab}>JSON</button>
+                </li>
+                <li className="nav-item">
+                  <button className={"nav-link code-tab btn " + (this.state.tabIndex === 1 ? 'active': null)} data-index="1" onClick={this.changeTab}>HTML</button>
+                </li>
+              </ul>
+            </div>
+            <div className="card-body">
+              {this.state.tabIndex === 0 ? (
+                <div>
+                  <h4>Stripe JSON</h4>
+                  <pre>
+                    <code>
+                      {toPrint}
+                    </code>
+                  </pre>
+                </div>
+              ) : (
+                <div>HTML</div>
+              )
+
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
