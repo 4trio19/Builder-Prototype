13fc201aee7f3c3e918176f106ee5f218cb3b6d2
4rio19 <nburressivc@gmail.com>
2019-10-11 14:54:03 -0500

    clean up json output

--- src/components/OutputBox.js
+++ src/components/OutputBox.js
@@ -2,10 +2,28 @@ import React from 'react';
 import PreviewBox from './PreviewBox';
 import CodeBox from './CodeBox';
 export default function OutputBox(props) {
+  let {...stripeDetails} = props.details;
+  delete stripeDetails.stripes;
+  delete stripeDetails.domain;
+  delete stripeDetails.esp;
+  delete stripeDetails.rows;
+  delete stripeDetails.stripeType;
+  delete stripeDetails.rows;
+  delete stripeDetails.stripeWidth;
+  delete stripeDetails.stripeID;
+  stripeDetails.articleCount = stripeDetails.columns;
+  delete stripeDetails.columns;
+  stripeDetails.isOpenRTB = false;
+  stripeDetails.dfpConfig = null;
+  let stripeOutput = {
+    id: props.details.stripeID,
+    stripe: {...stripeDetails}
+  };
+  console.log(stripeDetails);
   return (
     <div className="output-box container">
       <PreviewBox />
-      <CodeBox details={props.details} />
+      <CodeBox details={stripeOutput} />
     </div>
   )
 }
